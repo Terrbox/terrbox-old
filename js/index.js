6063efecf023be42562e319e4b41f58b8f0acbe5
Terrbox <terrbox.online@gmail.com>
2016-06-09 00:01:00 +0200

    Menu on header and other stuff

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 
 function heightSlide(){
-	$(".slider").css("height", $(window).height()-80);
+	$(".slider").css("height", $(window).height()-$("header").height());
+	$(".slider").css("margin-top", $("header").height());
 }
 
 $( window ).load(function() {
+	$("#atech").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#tech").offset().top-100
+	    }, 1000);
+	});
+	$("#aother").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#other").offset().top-100
+	    }, 1000);
+	});
+	$("#acontact").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#contact").offset().top-100
+	    }, 1000);
+	});
+	$(".arepo").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#repo").offset().top-100
+	    }, 1000);
+	});
+
 	heightSlide();
 });
 
